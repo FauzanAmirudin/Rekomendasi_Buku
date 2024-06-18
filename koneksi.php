539bc1e43b58c8a6eea69930ab1022e1f6a14b95
FauzanAmirudin <fauzan223360@gmail.com>
2024-06-19 00:54:34 +0700

    Merapihkan File File

--- koneksi.php
+++
@@ -1,4 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost","root","","rekomendasi_buku") or die ('database tidak terhubung');
-
-?>
--- session.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    if($_SESSION['user'] == false){
-        header('location: login.php');
-    }
-?>
