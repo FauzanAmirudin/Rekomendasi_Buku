77f37a0de67c0f1f4e9c33d9e3cf680887dc12b6
FauzanAmirudin <fauzan223360@gmail.com>
2024-06-20 20:27:11 +0700

    merapihkan

--- database/rekomendasi_db.sql
+++ database/rekomendasi_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Waktu pembuatan: 17 Jun 2024 pada 02.10
+-- Waktu pembuatan: 20 Jun 2024 pada 07.43
 -- Versi server: 8.0.30
 -- Versi PHP: 8.3.7
 
@@ -37,7 +37,7 @@ CREATE TABLE `buku` (
   `penerbit` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `tahun` int NOT NULL,
   `favorit` int NOT NULL DEFAULT '0',
-  `penonton` decimal(8,4) NOT NULL,
+  `pembaca` decimal(8,4) NOT NULL,
   `halaman` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -45,18 +45,18 @@ CREATE TABLE `buku` (
 -- Dumping data untuk tabel `buku`
 --
 
-INSERT INTO `buku` (`buku_id`, `judul`, `rating`, `sinopsis`, `gambar`, `penulis`, `penerbit`, `tahun`, `favorit`, `penonton`, `halaman`) VALUES
-(2, 'Tentang Kamu', 5.9, 'Legenda dari Jawa Barat tentang seorang anak laki-laki bernama Sangkuriang yang mencari ibunya dan tanpa sengaja jatuh cinta padanya. Kisah ini penuh dengan tragedi dan kutukan.', 'TentangKamu.jpeg', 'Tere Liye', 'Gramedia Pustaka Utama', 2009, 1, 10.0610, '344 halaman'),
-(3, 'Laskar Pelangi', 8.1, 'Mengisahkan tentang perjuangan sepuluh anak dari keluarga miskin di Belitung untuk meraih mimpi mereka di sekolah dasar Muhammadiyah. Novel ini penuh dengan semangat, humor, dan nilai-nilai persahabatan yang mengharukan.', 'LaskarPelangi.jpeg', 'Andrea Hirata', 'Bentang Pustaka', 2005, 1, 9.1251, ''),
-(4, 'Atomic Habits', 6.4, 'Buku ini akan menjelaskan mengenai suatu hal yang kecil bisa berdampak besar pada kehidupan seseorang. Perubahan-perubahan kecil akan menjadikan sesuatu hal yang luar biasa. Caranya dengan melakukannya secara rutin dan telaten.\r\nSelain itu, dalam buku ini terdapat juga contoh nyata kebiasaan kecil yang memiliki dampak besar.', 'AtomicHabits.png', 'James Clear', 'Gramedia Pustaka Utama', 2019, 2, 6.8586, ''),
-(5, 'Berani Tidak Disukai', 6.7, 'Buku ini terdiri dari 5 bab yang mampu mengajarkan bagaimana seseorang mampu menentukan arah hidup. Selain itu, menuntun untuk bebas dari trauma masa lalu dan beban ekspektasi orang lain. \r\nKonsep dari buku ini lebih mengajak untuk berdiskusi dan tidak terkesan menggurui. Jadi, pembaca akan merasa nyaman dan membuka wawasan tentang bagaimana penerimaan diri.', 'BeraniTidakDisukai.jpg', 'Ichiro Kishimi dan Fumitake Koga', 'Gramedia Pustaka Utama', 2016, 0, 6.3919, ''),
-(6, 'Kambing Jantan', 7.0, 'Buku ini merupakan kumpulan cerita ringan yang menghibur dengan sentuhan humor khas Raditya Dika. Melalui gaya tulisan yang sederhana dan lucu, Raditya mampu menghadirkan berbagai kejadian kocak yang dialaminya di luar negeri, mulai dari kesulitan beradaptasi dengan budaya baru, hingga kejadian-kejadian lucu yang terjadi dalam kehidupan sehari-hari.', 'KambingJantan.jpeg', 'Raditya Dika', 'Gagas Media', 2005, 0, 6.3156, ''),
-(7, 'Seribu Wajah Ayah', 7.9, 'Seorang anak perempuan yang merantau ke luar negeri kembali ke rumah setelah ayahnya meninggal. Di sana, ia menemukan album foto yang berisi kenangan masa lalunya bersama sang ayah dan belajar memahami berbagai sisi ayahnya yang tidak pernah ia ketahui sebelumnya.', 'SeribuWajahAyah.png', 'Nurun Ala', 'Gramedia Pustaka Utama', 2020, 0, 5.8609, '240 halaman'),
-(8, 'Patuhi Rules!', 5.3, 'Kisah tentang sekelompok anak SMA yang terjebak dalam permainan berbahaya yang disebut \"Rules\". Permainan ini penuh misteri dan memaksa mereka untuk mengikuti berbagai aturan yang aneh dan berbahaya.', 'PatuhiRules.jpg', 'Luluk HF', 'Gramedia Pustaka Utama', 2018, 0, 5.5029, '256 halaman'),
-(9, 'Rumah Kaca', 6.5, 'Novel sejarah yang berlatar belakang masa penjajahan Belanda di Indonesia. Kisah ini menceritakan tentang perjuangan seorang pemuda bernama Kurniawan untuk melawan penjajah dan mencari jati dirinya.', 'RumahKaca.jpg', 'Pramoedya Ananta Toer', 'Lentera Dipantara', 2002, 0, 5.2534, '448 halaman'),
-(10, 'Novel Catatan Pembunuhan Sang Novelis (Malice)', 6.0, 'Novel misteri tentang seorang editor yang dituduh membunuh novelis terkenal. Kisah ini penuh dengan ketegangan dan plot twist yang mengejutkan.', 'NovelCatatanPembunuhanSangNovelis.jpg', 'Keigo Higashino', 'Gramedia Pustaka Utama', 2012, 0, 4.8916, '344 halaman'),
-(11, 'Perpustakaan Tengah Malam (The Midnight Library)', 7.9, 'Kisah tentang seorang wanita bernama Nora Seed yang merasa hidupnya gagal dan memutuskan untuk bunuh diri. Namun, di ambang kematian, dia menemukan sebuah perpustakaan ajaib yang berisi buku-buku tentang kehidupan alternatifnya.', 'TheMidnightLibrary.jpg', 'Matt Haig', 'Gramedia Pustaka Utama', 2020, 1, 4.7194, '320 halaman'),
-(12, 'Habibie & Ainun', 7.6, 'Film ini didasarkan pada memoar yang ditulis oleh Presiden ketiga Indonesia dan salah satu insinyur terkenal di dunia, B.J. Habibie tentang istrinya, Hasri Ainun Habibie.', 'habibie.jpg', 'Faozan Rizal', 'Reza Rahadian, Bunga Citra Lestari, Tio Pakusadewo', 2012, 0, 4.6012, ''),
+INSERT INTO `buku` (`buku_id`, `judul`, `rating`, `sinopsis`, `gambar`, `penulis`, `penerbit`, `tahun`, `favorit`, `pembaca`, `halaman`) VALUES
+(2, 'Tentang Kamu', 0.0, 'Legenda dari Jawa Barat tentang seorang anak laki-laki bernama Sangkuriang yang mencari ibunya dan tanpa sengaja jatuh cinta padanya. Kisah ini penuh dengan tragedi dan kutukan.', 'TentangKamu.jpeg', 'Tere Liye', 'Gramedia Pustaka Utama', 2009, 2, 0.0000, '344 halaman'),
+(3, 'Laskar Pelangi', 0.0, 'Mengisahkan tentang perjuangan sepuluh anak dari keluarga miskin di Belitung untuk meraih mimpi mereka di sekolah dasar Muhammadiyah. Novel ini penuh dengan semangat, humor, dan nilai-nilai persahabatan yang mengharukan.', 'LaskarPelangi.jpeg', 'Andrea Hirata', 'Bentang Pustaka', 2005, 3, 0.0000, '528 halaman'),
+(4, 'Atomic Habits', 0.0, 'Buku ini akan menjelaskan mengenai suatu hal yang kecil bisa berdampak besar pada kehidupan seseorang. Perubahan-perubahan kecil akan menjadikan sesuatu hal yang luar biasa. Caranya dengan melakukannya secara rutin dan telaten.\r\nSelain itu, dalam buku ini terdapat juga contoh nyata kebiasaan kecil yang memiliki dampak besar.', 'AtomicHabits.png', 'James Clear', 'Gramedia Pustaka Utama', 2019, 2, 0.0000, '356 Halaman'),
+(5, 'Berani Tidak Disukai', 0.0, 'Buku ini terdiri dari 5 bab yang mampu mengajarkan bagaimana seseorang mampu menentukan arah hidup. Selain itu, menuntun untuk bebas dari trauma masa lalu dan beban ekspektasi orang lain. \r\nKonsep dari buku ini lebih mengajak untuk berdiskusi dan tidak terkesan menggurui. Jadi, pembaca akan merasa nyaman dan membuka wawasan tentang bagaimana penerimaan diri.', 'BeraniTidakDisukai.jpg', 'Ichiro Kishimi dan Fumitake Koga', 'Gramedia Pustaka Utama', 2016, 1, 0.0000, '350 Halaman\r\n'),
+(6, 'Kambing Jantan', 0.0, 'Buku ini merupakan kumpulan cerita ringan yang menghibur dengan sentuhan humor khas Raditya Dika. Melalui gaya tulisan yang sederhana dan lucu, Raditya mampu menghadirkan berbagai kejadian kocak yang dialaminya di luar negeri, mulai dari kesulitan beradaptasi dengan budaya baru, hingga kejadian-kejadian lucu yang terjadi dalam kehidupan sehari-hari.', 'KambingJantan.jpeg', 'Raditya Dika', 'Gagas Media', 2005, 0, 0.0000, '192 Halaman'),
+(7, 'Seribu Wajah Ayah', 0.0, 'Seorang anak perempuan yang merantau ke luar negeri kembali ke rumah setelah ayahnya meninggal. Di sana, ia menemukan album foto yang berisi kenangan masa lalunya bersama sang ayah dan belajar memahami berbagai sisi ayahnya yang tidak pernah ia ketahui sebelumnya.', 'SeribuWajahAyah.png', 'Nurun Ala', 'Gramedia Pustaka Utama', 2020, 0, 0.0000, '240 halaman'),
+(8, 'Patuhi Rules!', 0.0, 'Kisah tentang sekelompok anak SMA yang terjebak dalam permainan berbahaya yang disebut \"Rules\". Permainan ini penuh misteri dan memaksa mereka untuk mengikuti berbagai aturan yang aneh dan berbahaya.', 'PatuhiRules.jpg', 'Luluk HF', 'Gramedia Pustaka Utama', 2018, 0, 0.0000, '256 halaman'),
+(9, 'Rumah Kaca', 0.0, 'Novel sejarah yang berlatar belakang masa penjajahan Belanda di Indonesia. Kisah ini menceritakan tentang perjuangan seorang pemuda bernama Kurniawan untuk melawan penjajah dan mencari jati dirinya.', 'RumahKaca.jpg', 'Pramoedya Ananta Toer', 'Lentera Dipantara', 2002, 0, 0.0000, '448 halaman'),
+(10, 'Novel Catatan Pembunuhan Sang Novelis (Malice)', 0.0, 'Novel misteri tentang seorang editor yang dituduh membunuh novelis terkenal. Kisah ini penuh dengan ketegangan dan plot twist yang mengejutkan.', 'NovelCatatanPembunuhanSangNovelis.jpg', 'Keigo Higashino', 'Gramedia Pustaka Utama', 2012, 0, 0.0000, '344 halaman'),
+(11, 'Perpustakaan Tengah Malam (The Midnight Library)', 0.0, 'Kisah tentang seorang wanita bernama Nora Seed yang merasa hidupnya gagal dan memutuskan untuk bunuh diri. Namun, di ambang kematian, dia menemukan sebuah perpustakaan ajaib yang berisi buku-buku tentang kehidupan alternatifnya.', 'TheMidnightLibrary.jpg', 'Matt Haig', 'Gramedia Pustaka Utama', 2020, 1, 0.0000, '320 halaman'),
+(12, 'Renjana', 0.0, 'Novel tentang cinta dan kehilangan yang berlatar belakang masa Orde Baru di Indonesia. Kisah ini menceritakan tentang dua orang yang jatuh cinta pada masa remaja dan harus berpisah karena situasi politik yang tidak stabil.', 'Renjana.jpg', 'Leila S. Chudori', 'Gramedia Pustaka Utama', 2018, 0, 0.0000, '320 Halaman'),
 (13, 'Pengabdi Setan', 6.5, 'Setelah meninggal karena penyakit aneh yang dideritanya selama 3 tahun, seorang ibu kembali ke rumah untuk menjemput anak-anaknya.', 'ps.jpg', 'Joko Anwar', 'Tara Basro, Bront Palarae, Dimas Aditya', 2017, 0, 4.2061, ''),
 (14, 'Warkop DKI Reborn: Jangkrik Boss! Part 2', 5.5, 'Petualangan Dono, Kasino, dan Indro berlanjut. Mereka harus mencari harta karun itu untuk membayar utangnya. Mereka melakukan perjalanan ke Malaysia sebagai tujuan pertama mereka, namun tas dengan kode harta karun tersebut ditukar dengan milik wanita Malaysia.', 'warkopreborn2.jpg', 'Anggy Umbara\r\n\r\n', 'Abimana Aryasatya, Vino G. Bastian, Tora Sudiro', 2017, 0, 4.0831, ''),
 (15, 'Badarawuhi Di Desa Penari', 6.3, 'Desa ini masih menyimpan banyak misteri. Sepotong demi sepotong misteri terungkap, termasuk teror dari entitas yang paling ditakuti yakni Badarawuhi.', 'badarawuhi.jpg', 'Kimo Stamboel', 'Aulia Sarah, Maudy Effrosina, Jourdy Pranata', 2024, 0, 4.0101, ''),
@@ -87,7 +87,11 @@ INSERT INTO `favorit` (`id`, `user_id`, `buku_id`) VALUES
 (40, 6, 3),
 (42, 6, 11),
 (45, 6, 2),
-(46, 9, 4);
+(47, 9, 2),
+(48, 9, 4),
+(49, 9, 3),
+(50, 9, 5),
+(51, 10, 3);
 
 -- --------------------------------------------------------
 
@@ -190,7 +194,33 @@ INSERT INTO `komentar` (`komentar_id`, `buku_id`, `komentar`, `tanggal_komentar`
 (7, 4, 'sadasd', '2024-05-27 14:25:22', 10, 6),
 (8, 4, 'Bagus bro', '2024-05-27 14:28:31', 7, 3),
 (9, 2, 'sadasdasd', '2024-05-28 04:31:13', 10, 6),
-(10, 3, 'keren', '2024-05-29 05:21:26', 8, 6);
+(10, 3, 'keren', '2024-05-29 05:21:26', 8, 6),
+(11, 3, 'kacau\r\n', '2024-06-19 07:11:49', 10, 9);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `readlist`
+--
+
+CREATE TABLE `readlist` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `buku_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `readlist`
+--
+
+INSERT INTO `readlist` (`id`, `user_id`, `buku_id`) VALUES
+(44, 6, 4),
+(49, 6, 5),
+(53, 6, 2),
+(54, 9, 3),
+(57, 9, 4),
+(58, 9, 5),
+(59, 10, 4);
 
 -- --------------------------------------------------------
 
@@ -214,30 +244,8 @@ INSERT INTO `user` (`id`, `username`, `password`, `nama`) VALUES
 (4, 'admin1', '$2y$10$So0jDABohixT9v.UVTDO9.lKZ/rkSweTyL0xzGNkfl/S8QY7DCZLa', 'Admin1'),
 (6, 'vino', '$2y$10$EgBFRsrpS7SsGVeFojesWuPLUu9VOoN0fJIqhd6AKdX9vJh1TNsOS', 'Alvino'),
 (8, 'baron', '$2y$10$57oyJBtCydeLVfuJjqYTuusoUCtqu2RXQhkhlPk94e2yklMkBc6Hu', 'akbar'),
-(9, 'fauzan', '$2y$10$ecG37eMrFfdbjOMwsULy3.tCwyqyKmeg5iTygdLT4VfLL.rb8r3Ja', 'Fauzan');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `watchlist`
---
-
-CREATE TABLE `watchlist` (
-  `id` int NOT NULL,
-  `user_id` int NOT NULL,
-  `buku_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data untuk tabel `watchlist`
---
-
-INSERT INTO `watchlist` (`id`, `user_id`, `buku_id`) VALUES
-(44, 6, 4),
-(49, 6, 5),
-(53, 6, 2),
-(54, 9, 3),
-(55, 9, 2);
+(9, 'fauzan', '$2y$10$ecG37eMrFfdbjOMwsULy3.tCwyqyKmeg5iTygdLT4VfLL.rb8r3Ja', 'Fauzan'),
+(10, 'rosdiyanna', '$2y$10$f9/G7JOJ85jU2IEipaSh5eqLSwlRvAUf2RNK6pEr9kvTfEXCyV6rm', 'Rosdiyanna Safitri');
 
 --
 -- Indexes for dumped tables
@@ -255,14 +263,14 @@ ALTER TABLE `buku`
 ALTER TABLE `favorit`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`),
-  ADD KEY `buku_id` (`buku_id`);
+  ADD KEY `film_id` (`buku_id`);
 
 --
 -- Indeks untuk tabel `genre`
 --
 ALTER TABLE `genre`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `buku_id` (`buku_id`),
+  ADD KEY `film_id` (`buku_id`),
   ADD KEY `genre_id` (`genre_id`);
 
 --
@@ -276,22 +284,22 @@ ALTER TABLE `genres`
 --
 ALTER TABLE `komentar`
   ADD PRIMARY KEY (`komentar_id`),
-  ADD KEY `buku_id` (`buku_id`),
+  ADD KEY `film_id` (`buku_id`),
   ADD KEY `fk_user_id` (`user_id`);
+
+--
+-- Indeks untuk tabel `readlist`
+--
+ALTER TABLE `readlist`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `film_id` (`buku_id`);
 
 --
 -- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
-
---
--- Indeks untuk tabel `watchlist`
---
-ALTER TABLE `watchlist`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `buku_id` (`buku_id`);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
@@ -307,7 +315,7 @@ ALTER TABLE `buku`
 -- AUTO_INCREMENT untuk tabel `favorit`
 --
 ALTER TABLE `favorit`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 
 --
 -- AUTO_INCREMENT untuk tabel `genre`
@@ -325,19 +333,19 @@ ALTER TABLE `genres`
 -- AUTO_INCREMENT untuk tabel `komentar`
 --
 ALTER TABLE `komentar`
-  MODIFY `komentar_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `komentar_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT untuk tabel `readlist`
+--
+ALTER TABLE `readlist`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- AUTO_INCREMENT untuk tabel `watchlist`
---
-ALTER TABLE `watchlist`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
@@ -365,11 +373,11 @@ ALTER TABLE `komentar`
   ADD CONSTRAINT `komentar_ibfk_1` FOREIGN KEY (`buku_id`) REFERENCES `buku` (`buku_id`);
 
 --
--- Ketidakleluasaan untuk tabel `watchlist`
+-- Ketidakleluasaan untuk tabel `readlist`
 --
-ALTER TABLE `watchlist`
-  ADD CONSTRAINT `watchlist_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
-  ADD CONSTRAINT `watchlist_ibfk_2` FOREIGN KEY (`buku_id`) REFERENCES `buku` (`buku_id`);
+ALTER TABLE `readlist`
+  ADD CONSTRAINT `readlist_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `readlist_ibfk_2` FOREIGN KEY (`buku_id`) REFERENCES `buku` (`buku_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
